d2e1720e964b1f7ff5c5b9638c92c1b891319170
AnaMargarida13 <149785844+AnaMargarida13@users.noreply.github.com>
2024-03-22 13:13:12 +0100

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,14 @@
+/**
+ * Dies ist der Einstiegspunkt in unsere Applikation.<br/>
+ * Die Methode macht folgendes:
+ * <ol>
+ *   <li>Hallo Welt schreiben</li>
+ *   <li>Einen schönen Tag wünschen</li></ol>
+ *
+ * * @param args Kommandozeilenparameter
+ */
+
+
 public class Main {
 
     public static void main(String[] args) {
